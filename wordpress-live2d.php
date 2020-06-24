1bbd903944e44ea875d942c242fdb7140b067288
jiangweifang <38683169+jiangweifang@users.noreply.github.com>
2020-06-24 17:38:19 +0800

    修正了依赖项，不再引用整个JQUERY IU库，只引用jquery-ui-draggable类库，并使用WP自带类库

--- wordpress-live2d.php
+++ wordpress-live2d.php
@@ -7,20 +7,24 @@
  * Author: Chiang Weifang
  * Author URI: https://github.com/jiangweifang/wp-live2d
  */
+
+
 //定义目录
 define( 'LIVE2D_ASSETS', plugin_dir_url( __FILE__ ) . 'assets/' );//资源目录
 define('LIVE2D_BASEFOLDER', plugin_basename(dirname(__FILE__)));//基础目录
 define('LIVE2D_SETTINGPAGE', plugin_basename(__FILE__));//设置页面
 
+// 加载设置组件
+require(dirname(__FILE__)  . '/src/live2d-Main.php');
+
 //添加样式（初始化）
 
 function live2D_style(){
 	wp_enqueue_style( 'waifu_css' ,LIVE2D_ASSETS . "waifu.css");//css
-	wp_enqueue_script( 'jquery');//js
-	wp_enqueue_script( 'jquery-ui' ,LIVE2D_ASSETS.'jquery-ui.min.js');//js
-	wp_enqueue_script( 'waifu-tips_js' ,LIVE2D_ASSETS.'waifu-tips.js');//js
-	wp_enqueue_script( 'live2d_js' ,LIVE2D_ASSETS.'live2d.js');//js
-	
+    wp_enqueue_script( 'jquery');
+    wp_enqueue_script( 'jquery-ui-draggable');
+    wp_enqueue_script( 'live2d_js' ,LIVE2D_ASSETS.'live2d.js',array('jquery-ui-draggable'));
+	wp_enqueue_script( 'waifu-tips_js' ,LIVE2D_ASSETS.'waifu-tips.js',array('live2d_js'));
 }
 add_action( 'get_header', 'live2D_style' );
 
@@ -49,8 +53,7 @@ function live_2d_uninstall(){
     delete_option( 'live_2d_advanced_option_name' );
 }
 
-// 加载设置组件
-require(dirname(__FILE__)  . '/src/live2d-Main.php');
+
 
 
 // 实例化设置组件
@@ -60,28 +63,27 @@ if ( is_admin() ){
 
 //进行设置
 function live2D_DefMod(){
-// Retrieve this value with:
-$live_2d__options = get_option( 'live_2d_settings_option_name' ); // Array of All Options
-?>
-	<div class="waifu">
-        <div class="waifu-tips"></div>
-        <canvas id="live2d" class="live2d"></canvas>
-        <div class="waifu-tool">
-            <span class="fui-home"></span>
-            <span class="fui-chat"></span>
-            <span class="fui-eye"></span>
-            <span class="fui-user"></span>
-            <span class="fui-photo"></span>
-            <span class="fui-info-circle"></span>
-            <span class="fui-cross"></span>
+    // Retrieve this value with:
+    $live_2d__options = get_option( 'live_2d_settings_option_name' ); // Array of All Options
+    ?>
+        <div class="waifu">
+            <div class="waifu-tips"></div>
+            <canvas id="live2d" class="live2d"></canvas>
+            <div class="waifu-tool">
+                <span class="fui-home"></span>
+                <span class="fui-chat"></span>
+                <span class="fui-eye"></span>
+                <span class="fui-user"></span>
+                <span class="fui-photo"></span>
+                <span class="fui-info-circle"></span>
+                <span class="fui-cross"></span>
+            </div>
         </div>
-    </div>
-	<script type="text/javascript">
-			var settings_Json = '<?php echo json_encode($live_2d__options); ?>';
-        initModel("<?php echo LIVE2D_ASSETS ?>waifu-tips.json",JSON.parse(settings_Json));
-    </script>
-<?php
-		
+        <script type="text/javascript">
+            var settings_Json = '<?php echo json_encode($live_2d__options); ?>';
+           initModel("<?php echo LIVE2D_ASSETS ?>waifu-tips.json",JSON.parse(settings_Json));
+        </script>
+    <?php
 }
 add_action( 'wp_footer', 'live2D_DefMod' );
 
